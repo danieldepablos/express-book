5e908271f8bf83eede0b0b42ec8f4851d3f40548
Daniel Depablos <ddepabloss@gmail.com>
2019-09-04 13:33:25 -0400

    Fazt Code Youtube Tutorial

---
+++ src/routes/index.js
@@ -0,0 +1,53 @@
+const { Router } = require('express');
+const router = Router();
+const fs = require('fs');
+const uuid = require('uuid/v4');
+
+const json_books = fs.readFileSync('src/books.json', 'utf-8');
+let books = JSON.parse(json_books);
+
+router.get('/', (req, res) => {
+    res.render('index.ejs', {
+        books
+    })
+});
+
+router.get('/new-entry', (req, res) => {
+    res.render('new-entry');
+});
+
+router.post('/new-entry', (req, res) => {
+
+    const {title, author, image, description} = req.body;
+    if( !title || !author || !image || !description){
+        res.status(400).send('Entries must have a title and description');
+        return;
+    }
+    let newBook = {
+        id: uuid(),
+        title,
+        author,
+        image,
+        description
+    };
+
+    books.push(newBook);
+   
+    const json_books = JSON.stringify(books);
+
+    fs.writeFileSync('src/books.json' , json_books, 'utf-8');    
+    
+    res.send('Book Save!!');   
+});
+
+
+router.get('/delete/:id', (req, res) => {
+    //console.log(req.params);
+    //res.send('received');
+    books = books.filter(book => book.id != req.params.id);
+    const json_books = JSON.stringify(books);
+    fs.writeFileSync('src/books.json' , json_books, 'utf-8');    
+    res.redirect('/');
+});
+
+module.exports = router;
